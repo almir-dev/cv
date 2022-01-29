65eb53cf406035a898faa8a78799e73c737f1a25
almir <almir.omerovic.dev@gmail.com>
2022-01-29 15:26:50 +0100

    add personal website in contact information

--- src/components/cv/basic/Contact.tsx
+++ src/components/cv/basic/Contact.tsx
@@ -1,30 +1,47 @@
 import React from "react";
-import {InfoSection} from "../core/info-section/InfoSection";
-import {LabeledField} from "../core/labeled-field/LabeledField";
-import {Email, LinkedIn, GitHub} from "@material-ui/icons";
+import { InfoSection } from "../core/info-section/InfoSection";
+import { LabeledField } from "../core/labeled-field/LabeledField";
+import { Email, LinkedIn, GitHub, Web } from "@material-ui/icons";
 
 export function Contact() {
-    return <InfoSection label={'Contact'}>
-        <EmailField/>
-        <LinkedInField/>
-        <GithubField/>
+  return (
+    <InfoSection label={"Contact"}>
+      <EmailField />
+      <LinkedInField />
+      <GithubField />
+      <WebField />
     </InfoSection>
+  );
 }
 
 export function EmailField() {
-    return <LabeledField icon={<Email/>} label={'Email'}>
-        almir.omerovic.dev@gmail.com
+  return (
+    <LabeledField icon={<Email />} label={"Email"}>
+      almir.omerovic.dev@gmail.com
     </LabeledField>
+  );
 }
 
 export function LinkedInField() {
-    return <LabeledField icon={<LinkedIn/>} label={'LinkedIn'}>
-        www.linkedin.com/aomerovi
+  return (
+    <LabeledField icon={<LinkedIn />} label={"LinkedIn"}>
+      www.linkedin.com/aomerovi
     </LabeledField>
+  );
 }
 
 export function GithubField() {
-    return <LabeledField icon={<GitHub/>} label={'GitHub'}>
-        www.github.com/almir-dev
+  return (
+    <LabeledField icon={<GitHub />} label={"GitHub"}>
+      www.github.com/almir-dev
     </LabeledField>
+  );
+}
+
+export function WebField() {
+  return (
+    <LabeledField icon={<Web />} label={"Website"}>
+      www.almir-dev.com
+    </LabeledField>
+  );
 }
